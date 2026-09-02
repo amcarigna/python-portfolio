0edf09ce3d8dd284f5088d0c1bfc9e850a6d5a52
acarigna <amcarigna@gmail.com>
2026-09-02 11:33:39 -0400

    add templates and projects list

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My Python Portfolio
+A portfolio website made with Flask. Made in a Treehouse course.
---
+++ projects.py
@@ -0,0 +1,17 @@
+projects = [
+    {
+        "image": "/static/images/stargazer-screenshot.png",
+        "name": "Stargazer Log",
+        "techs": [
+            "JavaScript",
+            "HTML",
+            "CSS"
+        ],
+        "description": """This project served as a introduction to using Git and Github. Before this, I was aware
+        generally of version control, why it is, and rough logic behind how it works. But using Git and Github always
+        intimidated me, and I avoided it. Now I am comfortable using its basic features and ready to learn other
+        ones.""",
+        "live": "https://amcarigna.github.io/stargazers-log/",
+        "repo": "https://github.com/amcarigna/stargazers-log"
+    }
+]
